7e589effbeb227a56098981f5ce9f54dcbbb2f9e
HoangTemp <hoangtam123456728@gmail.com>
2025-06-10 23:56:36 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$database = "banhang_mvc";
+$database = "banhang";
 $conn = mysqli_connect($host, $user, $password, $database);
 if (mysqli_connect_error()){
     echo "Connection Fail: ".mysqli_connect_error();exit;
